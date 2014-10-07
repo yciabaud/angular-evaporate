667b6a16558980e527315e4f0aed2568d82b5613
Denis Zhbankov <denis.zhbankov@gmail.com>
2014-10-08 01:48:32 +0400

    remove uploaded files from evaporate ng-model

--- example/index.js
+++ example/index.js
@@ -2,6 +2,7 @@
 angular
   .module('app', ['evaporate'])
   .controller('AppCtrl', ['$scope', function ($scope) {
+    $scope.uploadedFiles = [];
 
     // this variable is used like a model for particular directive
     // all parameters here are optional
@@ -11,7 +12,7 @@ angular
       // http://<your_bucket>.s3.amazonaws.com/<this_value>/<upload_datetime>$<filename_with_extension>
       // if you want to put files into nested folder, just use dir: 'path/to/your/folder'
       // if not specified, default value being used is: '' (matches bucket's root directory)
-      dir: 'tmp',
+      dir: 'img/tmp',
 
       // headers which should (headersSigned) and should not (headersCommon) be signed by your private key
       // for details, visit http://docs.aws.amazon.com/AmazonS3/latest/API/RESTObjectPUT.html
@@ -33,6 +34,7 @@ angular
         );
       },
       onFileComplete: function (file) {
+        $scope.uploadedFiles.push(file);
         console.log('onComplete || name: %s', file.name);
       }
     };
